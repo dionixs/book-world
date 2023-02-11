3942f390341a10adf75c2efb4abb66105aa6f3e1
di0nixs <di0nixs@protonmail.com>
2023-02-12 01:05:41 +0300

    Add basic validations for Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :description, presence: true
+  validates :genre, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :cover, presence: true
 end
