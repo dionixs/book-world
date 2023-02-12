aad989605c39356b4a735a293a3bdd12b78318cf
di0nixs <di0nixs@protonmail.com>
2023-02-12 13:56:49 +0300

    Add test for creating a Book object 📚

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 require 'shoulda/matchers'
 
 RSpec.describe Book, type: :model do
+
   describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:author) }
@@ -11,5 +12,12 @@ RSpec.describe Book, type: :model do
     it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(0) }
     it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
     it { should validate_presence_of(:cover) }
+  end
+
+  describe 'creating a book' do
+    it 'creates a valid book' do
+      book = Book.new(title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', description: 'A classic novel about the decadence of the Roaring Twenties', genre: 'Literary Fiction', rating: 4, cover: 'https://example.com/the-great-gatsby.jpg')
+      expect(book).to be_valid
+    end
   end
 end
