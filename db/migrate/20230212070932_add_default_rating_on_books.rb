e32357e930cf817131092a314d4e8872b0a270b7
di0nixs <di0nixs@protonmail.com>
2023-02-12 11:28:57 +0300

    Set Default Rating for Books to 0 in Database Migration

---
+++ db/migrate/20230212070932_add_default_rating_on_books.rb
@@ -0,0 +1,9 @@
+class AddDefaultRatingOnBooks < ActiveRecord::Migration[7.0]
+  def up
+    change_column_default(:books, :rating, 0)
+  end
+
+  def down
+    change_column_default :books, :rating, nil
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_11_211533) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_12_070932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_11_211533) do
     t.string "author"
     t.text "description"
     t.string "genre"
-    t.integer "rating"
+    t.integer "rating", default: 0
     t.string "cover"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
