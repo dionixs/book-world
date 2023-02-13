d74c5859e9da210e3c849342257e3597b241b4c5
di0nixs <di0nixs@protonmail.com>
2023-02-13 22:29:48 +0300

    🔧 Fix Book model validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,5 +6,5 @@ class Book < ApplicationRecord
   validates :author, presence: true
   validates :description, presence: true
   validates :genre, presence: true
-  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :rating, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 5.0 }
 end
