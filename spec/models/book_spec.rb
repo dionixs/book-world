6f05b3cd13af183bdb712b8826713bf4d1dd6f9c
di0nixs <di0nixs@protonmail.com>
2023-02-13 22:27:14 +0300

    Update book_spec.rb

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -8,9 +8,7 @@ RSpec.describe Book, type: :model do
     it { should validate_presence_of(:author) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:genre) }
-    it { should validate_numericality_of(:rating).only_integer }
-    it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(0) }
-    it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
+    it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(0.0).is_less_than_or_equal_to(5.0) }
   end
 
   describe "#set_default_cover" do
