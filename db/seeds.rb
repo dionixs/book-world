900c356a43edae4ccdc84decc520a1cea05fb698
di0nixs <di0nixs@protonmail.com>
2023-02-13 22:35:29 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Book.create([
+              { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', description: 'A novel about decadence, idealism, resistance to change, and social upheaval', genre: 'Literary Fiction', rating: 4.5, cover: 'https://images-na.ssl-images-amazon.com/images/I/51jNORv5BkL._SX329_BO1,204,203,200_.jpg' },
+              { title: 'Pride and Prejudice', author: 'Jane Austen', description: 'A novel about manners, upbringing, morality, education, and marriage in Georgian England', genre: 'Classics', rating: 3.5, cover: 'https://images-na.ssl-images-amazon.com/images/I/51q0j0Sb%2BRL._SX331_BO1,204,203,200_.jpg' },
+              { title: 'To Kill a Mockingbird', author: 'Harper Lee', description: 'A novel about racial injustice, childhood innocence, and the loss of innocence', genre: 'Classics', rating: 2.5, cover: 'https://images-na.ssl-images-amazon.com/images/I/51uA9L8c%2BFL._SX324_BO1,204,203,200_.jpg' }
+            ])
