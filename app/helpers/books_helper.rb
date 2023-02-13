78a3cddeaa7fed83adb7c3d21faf9e4d8616e997
di0nixs <di0nixs@protonmail.com>
2023-02-13 22:32:36 +0300

    Implement star rating display for book ratings with bootstrap icons

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,13 @@
 module BooksHelper
+  def stars_for_rating(rating)
+    (1..5).map do |i|
+      if rating >= i
+        content_tag(:i, nil, class: 'bi bi-star-fill text-warning fs-4 me-1')
+      elsif rating > (i - 1) && rating < i
+        content_tag(:i, nil, class: 'bi bi-star-half text-warning fs-4 me-1')
+      else
+        content_tag(:i, nil, class: 'bi bi-star fs-4 me-1')
+      end
+    end.join.html_safe
+  end
 end
