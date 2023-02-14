7aef7b55fbcc9ce48eeb6a44cedadb6e78b733a0
di0nixs <di0nixs@protonmail.com>
2023-02-14 20:14:11 +0300

    Add a book edit icon, which should be a link leading to books/:id/edit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,5 @@
 // Entry point for the build script in your package.json
 import {Turbo} from "@hotwired/turbo-rails"
-
 Turbo.session.drive = false
 // import "./controllers"
 import * as bootstrap from "bootstrap"
-
-
