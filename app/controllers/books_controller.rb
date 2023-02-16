75ba2174c66a33ca3c742088a0a99251ca87cde5
di0nixs <di0nixs@protonmail.com>
2023-02-16 17:35:16 +0300

    Remove genre in book_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :set_book, only: %i[show edit update destroy]
 
   def index
     @books = Book.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @book = Book.new
@@ -22,8 +23,7 @@ class BooksController < ApplicationController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @book.update(book_params)
@@ -45,7 +45,6 @@ class BooksController < ApplicationController
   end
 
   def book_params
-    params.require(:book).permit(:title, :author, :description,
-                                 :genre, :rating, :cover)
+    params.require(:book).permit(:title, :author, :description, :rating, :cover)
   end
 end
