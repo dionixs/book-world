7fa3513408473628baa07e90222839bfe8805b06
di0nixs <di0nixs@protonmail.com>
2023-02-19 21:35:09 +0300

    Add helper full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = 'BookWorld'
+    if page_title.empty?
+      base_title
+    else
+      page_title.to_s
+    end
+  end
 end
