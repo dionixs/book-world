b79f10dd814197c822df29f943b2a647c79f72ea
di0nixs <di0nixs@protonmail.com>
2023-02-22 11:25:53 +0300

    Add index on created_at field in reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_22_081718) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_22_082508) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_081718) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["created_at"], name: "index_reviews_on_created_at"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
