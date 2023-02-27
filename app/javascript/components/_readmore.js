523295c3b87b0189f013ac4e127ce7f0ec0e22e4
di0nixs <di0nixs@protonmail.com>
2023-02-27 14:22:46 +0300

    Update show/hide text functionality on 'Read more...' button click

--- app/javascript/components/_readmore.js
+++ app/javascript/components/_readmore.js
@@ -1,21 +1,22 @@
 export function initReadMoreButton(textSelector, buttonSelector, maxTextLength) {
   const textElements = document.querySelectorAll(textSelector);
-  textElements.forEach((textElement) => {
-    const text = textElement.textContent;
+
+  textElements.forEach(textElement => {
+    const text = textElement.textContent.trim();
     if (text.length > maxTextLength) {
-      const trimmedText = text.slice(0, maxTextLength);
-      const remainingText = text.slice(maxTextLength);
-      textElement.innerHTML = `${trimmedText}<span class="remaining-text" style="display:none">${remainingText}</span>`;
-      const readMoreButton = textElement.parentNode.querySelector(buttonSelector);
-      readMoreButton.addEventListener('click', (event) => {
-        event.preventDefault();
-        const remainingTextElement = textElement.querySelector('.remaining-text');
-        if (remainingTextElement.style.display === 'none') {
-          remainingTextElement.style.display = 'inline';
-          readMoreButton.textContent = 'Read less...';
+      const truncatedText = text.slice(0, maxTextLength) + '...';
+      textElement.textContent = truncatedText;
+
+      const button = textElement.parentElement.querySelector(buttonSelector);
+      button.addEventListener('click', () => {
+        if (textElement.classList.contains('read-more-expanded')) {
+          textElement.textContent = truncatedText;
+          textElement.classList.remove('read-more-expanded');
+          button.textContent = 'Read more...';
         } else {
-          remainingTextElement.style.display = 'none';
-          readMoreButton.textContent = 'Read more...';
+          textElement.textContent = text;
+          textElement.classList.add('read-more-expanded');
+          button.textContent = 'Read less...';
         }
       });
     }
