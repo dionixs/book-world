3c54409e98c500ca64a4ed176a87da21f30d6333
di0nixs <di0nixs@protonmail.com>
2023-02-28 17:39:34 +0300

    Update flash messages use flash_class helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
   include ErrorHandling
   include Pagy::Backend
-
-  add_flash_types :info, :danger, :success
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,13 @@ module ApplicationHelper
     plain_text = strip_tags(object.send(attribute).to_plain_text)
     truncate(plain_text, length: length)
   end
+
+  def flash_class(level)
+    case level.to_sym
+      when :notice then "alert alert-info"
+      when :success then "alert alert-success"
+      when :error then "alert alert-danger"
+      when :alert then "alert alert-danger"
+    end
+  end
 end
