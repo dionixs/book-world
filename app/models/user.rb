8ec90a77fad02064e948bc5ba29ff7ce690c23b3
di0nixs <di0nixs@protonmail.com>
2023-03-01 13:02:11 +0300

    Set up simple password complexity requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include PasswordComplexityValidator
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :password_complexity
 end
