71bd98ced0e1c8d41e3436d3fea3ad2bb4a43ce7
di0nixs <di0nixs@protonmail.com>
2023-03-01 18:40:31 +0300

    Add username field to User model and validate it

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, presence: true, length: { minimum: 3, maximum: 25 },
+                       uniqueness: { case_sensitive: false,
+                                     message: 'An account associated with %<value>s already exists' },
+                       exclusion: { in: %w[admin root superuser] }
+  validates_format_of :username, with: /\A[a-zA-Z0-9_]+\z/i
   validate :password_complexity
 end
