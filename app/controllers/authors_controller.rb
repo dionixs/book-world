c7a85c8ffc4cea26814646a0299ddaa4414335d2
di0nixs <di0nixs@protonmail.com>
2023-03-01 12:02:40 +0300

    Update all books page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,6 +7,7 @@ class AuthorsController < ApplicationController
   # GET /authors or /authors.json
   def index
     @authors = Author.all
+    @pagy, @authors = pagy(@authors, items: 30)
   end
 
   # GET /authors/1 or /authors/1.json
--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 module AuthorsHelper
+  include ImageHelper
+
+  def author_photo(obj, default_cover: 'default_author_photo.jpg', cover_method: :photo)
+    super
+  end
 end
