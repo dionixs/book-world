db41cad33cd7e5d48f8c5f7ca09cc497c8ac7bf3
di0nixs <di0nixs@protonmail.com>
2023-03-02 09:08:33 +0300

    Allow users to recover their password or confirm their account using their username

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,13 +15,14 @@ class User < ApplicationRecord
   validate :validate_username
   validate :password_complexity
 
-  def self.find_for_database_authentication(warden_conditions)
+  def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if (login = conditions.delete(:login))
-      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value',
-                                    { value: login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    elsif conditions[:username].nil?
+      where(conditions).first
+    else
+      where(username: conditions[:username]).first
     end
   end
 end
