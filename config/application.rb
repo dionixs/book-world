40e18b140d6ce8a36ceea795fb7b60303a910822
di0nixs <di0nixs@protonmail.com>
2023-03-08 20:12:14 +0300

    Add available locales to i18n

--- config/application.rb
+++ config/application.rb
@@ -25,11 +25,8 @@ module BookWorld
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
+    config.i18n.available_locales = %i[en ru]
+
     config.time_zone = 'Moscow'
     # config.eager_load_paths << Rails.root.join("extras")
 
