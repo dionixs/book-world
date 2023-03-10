4810f3c4de5f3631e84971e451f2191624404623
di0nixs <di0nixs@protonmail.com>
2023-03-10 17:18:38 +0300

    Fix undefined method `profile' for nil:NilClass

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_profile, only: %i[edit update]
 
   def edit; end
@@ -20,6 +21,6 @@ class ProfilesController < ApplicationController
   end
 
   def set_profile
-    @profile = current_user.profile
+    @profile = current_user&.profile
   end
 end
