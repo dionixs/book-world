3d45ae235b936c53ec0a98f53e6ce29dd587914e
di0nixs <di0nixs@protonmail.com>
2023-03-13 13:34:26 +0300

    Add helper function for highlighting current page in header and footer links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,6 +26,18 @@ module ApplicationHelper
     end
   end
 
+  def nav_tab(title, url, options = {})
+    css_class = current_page?(url) ? 'nav-link__orange' : 'nav-link__white'
+
+    options[:class] = if options[:class]
+                        "#{options[:class]} #{css_class}"
+                      else
+                        css_class
+                      end
+
+    link_to title, url, options
+  end
+
   def summary(object, attribute, options = {})
     length = options[:length] || 200
     plain_text = plain_text(object, attribute)
