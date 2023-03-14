4d0dd011c5cb1c3c46346e2dd7ab6a853f46049f
di0nixs <di0nixs@protonmail.com>
2023-03-14 16:22:54 +0300

    Update resource users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,15 +1,29 @@
 # frozen_string_literal: true
 
-ActiveAdmin.register User do
-  permit_params :username, :email
+ActiveAdmin.register User, as: 'Users' do
+  permit_params :username, :email, :password,
+                :password_confirmation, :tos_agreement
 
-  index do
+  index title: 'List of users' do
     selectable_column
     id_column
     column :username
     column :email
     column :created_at
     column :updated_at
+    column :tos_agreement
+    actions
+  end
+
+  form title: 'Create User' do |f|
+    inputs 'Details' do
+      f.input :username
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :tos_agreement
+    end
+    para 'Press cancel to return to the list without saving.'
     actions
   end
 end
