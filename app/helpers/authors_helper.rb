71b5689f1a917cd4bca6749d437cf7c9e81090a9
di0nixs <di0nixs@protonmail.com>
2023-03-19 17:38:42 +0300

    Update view for authors

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -3,7 +3,10 @@
 module AuthorsHelper
   include ImageHelper
 
-  def author_photo(obj, default_cover: 'default_author_photo.jpg', cover_method: :photo)
+  # TODO
+  def author_photo(obj, default_cover: '/images/defaults/default_author_photo.jpg',
+                   cover_method: :photo,
+                   cover_method_url: :photo_url)
     super
   end
 end
