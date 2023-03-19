5d145856b4620f837fbc807480cf1aaa4d700017
di0nixs <di0nixs@protonmail.com>
2023-03-19 17:39:21 +0300

    Update BookDecorator

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -3,8 +3,9 @@
 class BookDecorator < ApplicationDecorator
   delegate_all
 
+  # TODO
   def short_author_name
-    author_name = authors.first&.name
+    author_name = authors.first&.short_name
     if author_name.present?
       author_name.length > 20 ? "#{author_name[0..19]}..." : author_name
     else
