bbef8ecdfd3dd491aaae8f84305b0fe0ea35ad78
di0nixs <di0nixs@protonmail.com>
2023-03-19 11:06:46 +0300

    Update book_cover method

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 module ImageHelper
-  def book_cover(obj, default_cover: 'default_cover.jpg', cover_method: :cover)
+  # TODO
+  def book_cover(obj, default_cover: '/images/defaults/default_cover.jpg',
+                 cover_method: :cover,
+                 cover_method_url: :cover_url)
+    return url_for(obj.send(cover_method_url)) if obj.send(cover_method_url) != ''
+
     if obj.respond_to?(cover_method) && obj.send(cover_method).attached?
       obj.send(cover_method)
     else
-      default_cover
+      url_for(default_cover)
     end
   end
 
