fed90b6b7a9511ef490b4353c991e258e3548320
di0nixs <di0nixs@protonmail.com>
2023-03-21 20:05:49 +0300

    Add letter_tab helper

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -17,9 +17,9 @@ module AuthorsHelper
   # TODO
   def initial_letters_range
     if I18n.locale == :ru
-      ('А'..'Я').select { |letter| letter != 'Ъ' && letter != 'Ь' }
+      ('а'..'я').select { |letter| letter != 'ъ' && letter != 'ь' }
     else
-      ('A'..'Z')
+      ('a'..'z')
     end
   end
 
@@ -44,5 +44,17 @@ module AuthorsHelper
         name
       end
     end.join(', ').html_safe
+  end
+
+  def letter_tab(title, url, options = {})
+    css_class = current_page?(url) ? 'active_letter' : ''
+
+    options[:class] = if options[:class]
+                        "#{options[:class]} #{css_class}"
+                      else
+                        css_class
+                      end
+
+    link_to title, url, options
   end
 end
