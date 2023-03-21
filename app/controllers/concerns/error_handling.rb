1ca3bac4a1280f69244f921668922fcc979a265c
di0nixs <di0nixs@protonmail.com>
2023-03-21 10:38:05 +0300

    Rename notfound to not_found

--- app/controllers/concerns/error_handling.rb
+++ app/controllers/concerns/error_handling.rb
@@ -4,11 +4,11 @@ module ErrorHandling
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveRecord::RecordNotFound, with: :notfound
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
     private
 
-    def notfound(exception)
+    def not_found(exception)
       logger.warn exception
       render file: 'public/404.html', status: :not_found, layout: false
     end
