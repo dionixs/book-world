31a59edf9ad8df3f036f95a7198ac99354d2a903
di0nixs <di0nixs@protonmail.com>
2023-03-28 09:46:13 +0300

    Move all database calls to controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,12 @@
 class SearchController < ApplicationController
   def search
     @query = params[:search]
+
     @books = Book.active.search(@query).includes([:cover_attachment]).includes([:authors]).limit(100)
+    @book_count = @books.count
     @books = @books.decorate
+
     @authors = Author.active.search(@query).includes([:photo_attachment]).limit(6)
+    @author_count = @authors.count
   end
 end
